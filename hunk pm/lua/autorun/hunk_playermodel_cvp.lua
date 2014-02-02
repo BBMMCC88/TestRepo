ebee266251f0dc36e8162795c299acca6c91cab6
Brandon Carrothers <bbmmcc88@comcast.net>
2014-02-02 15:42:27 -0500

    Test For GMOD Server
    This is just a test and not the final version

---
+++ hunk pm/lua/autorun/hunk_playermodel_cvp.lua
@@ -0,0 +1,5 @@
+player_manager.AddValidModel( "HUNK_cvp", 				"models/player/lordvipes/residentevil/HUNK/hunk_playermodel_cvp.mdl" )
+
+player_manager.AddValidHands( "HUNK_cvp", "models/player/lordvipes/residentevil/HUNK/hunkarms_cvp.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel",  "HUNK_cvp", 				"models/player/lordvipes/residentevil/HUNK/hunk_playermodel_cvp.mdl" )
